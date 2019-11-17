4da7d07c921fc5f910343206784d552599d7cc71
luuthin <luucongthin2019@gmail.com>
2019-11-18 00:18:21 +0700

    add ISpeak dashboard

---
+++ BackEnd/ISpeakDashboard/ISpeakDashboard/Models/UserInfoModel.cs
@@ -0,0 +1,12 @@
+﻿namespace ISpeakDashboard.Models
+{
+    public class UserInfoModel
+    {
+        public string Id { get; set; }
+        public string Username { get; set; }
+        public string Fullname { get; set; }
+        public string Email { get; set; }
+        public string Mobile { get; set; }
+        public string Avatar { get; set; }
+    }
+}
